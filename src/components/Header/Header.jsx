38f346389bf0a84635244c086090e94890ef7d77
Vinlasy <artem.tkachev.00@mail.ru>
2025-10-30 23:15:02 +0300

    рабочая версия

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import "../../styles/style.scss";
 import styles from "./Header.module.scss";
+import githubLogo from "../../assets/svg/github-logo.svg";
+import { useState } from "react";
 
 export const Header = () => {
     const mainNav = [
@@ -17,22 +19,44 @@ export const Header = () => {
         },
     ]
 
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const handleOpenBurger = () => setIsOpen((prev) => !prev);
+    const handleCloseBurger = () => setIsOpen(false);
+
     return (
         <header className={styles.header}>
             <div className="container">
                 <div className={styles.header__content}>
+                    <button 
+                        className={`${styles.header__burger} ${isOpen ? styles["header__burger--active"] : ""}`} 
+                        aria-label="Меню навигации" 
+                        onClick={handleOpenBurger}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
                     <nav className={styles.header__nav} aria-label="Основная">
-                        <ul className={styles.header__navList}>
+                        <ul className={`${styles.header__navList} ${isOpen ? styles["header__navList--active"] : ""}`}>
                             {mainNav.map((item) =>
                                 <li className={styles.header__navItem}>
-                                    <a className={styles.header__navLink} href={`#${item.anchorId}`} >
+                                    <a 
+                                        className={styles.header__navLink} 
+                                        href={`#${item.anchorId}`} 
+                                        onClick={handleCloseBurger}
+                                    >
                                         {item.text}
                                     </a>
                                 </li>
                             )}
                         </ul>
                     </nav>
-                    <a className={styles.header__btn} href="https://github.com/Vinlasy/my-portfolio.git" target="blank_">Репозиторий на GitHub</a>
+                    <a className={styles.header__btn} href="https://github.com/Vinlasy/my-portfolio.git" target="blank_">
+                        <img className={styles.header__btnIcon} src={githubLogo} alt="Иконка GitHub" width={24} height={24} aria-hidden={true}/>
+                        <span className={styles.header__btnText}>Репозиторий</span>
+                    </a>
                 </div>
             </div>
         </header>
